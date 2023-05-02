9afe8a3f948ea077c5b2cff0b8ccbdd09783900e
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-02 11:34:44 -0300

    Update ex009.js

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,7 @@
-var país = 'EUA'
+var país = 'Brasil'
 console.log(`Vivendo em ${país}`)
-if (país == 'Brasil') {
-    console.log('Você é Brasileiro!')
-} else {
+if (país != 'Brasil') {
     console.log('Você é estrangeiro!')
+} else {
+    console.log('Você é brasileiro!')
 }
