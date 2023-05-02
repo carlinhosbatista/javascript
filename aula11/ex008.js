c475ea38b18462b81be81b83bade8526030c5882
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-02 11:19:45 -0300

    Update ex008.js

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,1 +1,4 @@
-console.log('O console funcionou corretamente')
+var vel = 60.5
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Você ultrassou a velocidade permitida. MULTADO!`)
+console.log(`Dirija sempre usando cinto de segurança!`)
