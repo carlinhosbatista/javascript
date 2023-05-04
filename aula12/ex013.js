e75fa2fd85611c3b23a07a4e282585e7735f2296
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-04 14:29:17 -0300

    Create ex013.js

---
+++ aula12/ex013.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var diaSem = agora.getDay()
+/*
+    0 = Domingo
+    1 = Segunda
+    2 = Terça
+    3 = Quarta
+    4 = Quinta
+    5 = Sexta
+    6 = Sábado
+*/
+
+console.log(diaSem)
