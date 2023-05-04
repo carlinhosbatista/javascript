387f5cff1dd04a7dfbec3dd794c5ef4c53ec54cb
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-04 10:51:21 -0300

    Update ex011.js

--- aula12/ex011.js
+++ aula12/ex011.js
@@ -1,6 +1,6 @@
-var idade = 16
-if (idade < 18) {
+var idade = 22
+if (idade < 16) {
     console.log('Não vota')
-} else {
-    console.log('Vota')
-}
+} else if (idade < 18) {
+        console.log('Voto opcional')
+    }
