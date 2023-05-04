bcd4b5112f570ede3bb595f6ce085788cb34c494
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-04 11:17:34 -0300

    Create ex012.js

---
+++ aula12/ex012.js
@@ -0,0 +1,11 @@
+var hora = 21
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else if (hora <= 23) {
+    console.log('Boa Noite!')
+} else {
+    console.log('Durma Bem!')
+}
