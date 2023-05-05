fbb90fc528321f8c5dfd5000a077f8ec63b4d536
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-05 10:17:37 -0300

    Update script.js

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -4,4 +4,12 @@ function carregar() {
     var data = new Date()   
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA!
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE!
+    } else {
+        // BOA NOITE!
+    }
+    
 }
