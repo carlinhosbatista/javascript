f86ad1e2bd1c849847d1e8a5d8899dff349d8fb3
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-05 10:28:52 -0300

    Atualizei foto

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,14 +2,18 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem') 
     var data = new Date()   
-    var hora = data.getHours()
+    //var hora = data.getHours()
+    var hora = 17
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
         // BOM DIA!
+        img.src = 'fotomanha.png'
     } else if (hora >= 12 && hora < 18) {
         // BOA TARDE!
+        img.src = 'fototarde.png'
     } else {
         // BOA NOITE!
+        img.src = 'fotonoite.png'
     }
     
 }
