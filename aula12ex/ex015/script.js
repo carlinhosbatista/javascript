d0bfd25a916e8b477f1560f9bd5576ed201a72de
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-06 23:13:42 -0300

    Atualizei

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -2,12 +2,10 @@ function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
-    var res = document.querySelector('div#res')
-    if (fano.value.lenght == 0 || Number(fano.value) > ano) {
-        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    var res = document.getElementById('res')
+    if (fano.value == 0 || Number(fano.value) > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementsByName('radsex')
-        var idade = ano - Number(fano.value)
-    }
         
+    }
 }
