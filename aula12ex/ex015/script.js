7b528d2496694f7419dd0975dae8704c9a1c568b
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-06 21:09:35 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,5 +1,9 @@
 function verificar() {
     var data = new Date()
     var ano = data.getFullYear()
-    
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if(fano.value.lenght == 0 || fano.value > ano) {
+        alert('[ERRO] Verifique os dados e tente novamente!')
+    }
 }
