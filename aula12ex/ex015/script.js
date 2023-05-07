48231b9bf89df863937dfbaf1aa8185967553843
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-07 13:44:06 -0300

    Update script.js

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -6,6 +6,8 @@ function verificar() {
     if (fano.value == 0 || Number(fano.value) > ano) {
         alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
     }
 }
