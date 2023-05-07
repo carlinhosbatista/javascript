c678955f25234fb543eaee6d957798fb54f36c1f
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-07 15:03:22 -0300

    Atualizei

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -15,17 +15,36 @@ function verificar() {
             genero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 // Criança
+                img.setAttribute('src', 'foto-bebe-m.png')
             } else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 50) {
                 // Adulto
+                img.setAttribute('src', 'foto-adulto-m.png')
             } else {
                 // Idoso
+                img.setAttribute('src', 'foto-idoso-m.png')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'foto-bebe-f.png')
+            } else if (idade < 21) {
+                // Jovem
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
         }
+        
         res.style.textAlign = 'center'
-        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.innerHTML = `<p>Detectamos ${genero} com ${idade} anos.</p>`
+        res.appendChild(img)
     }
 }
