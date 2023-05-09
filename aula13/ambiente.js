c7ca4983cdcabb05c8946ff96953561266e0e92f
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-09 09:30:19 -0300

    Update ambiente.js

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,10 +1,16 @@
 var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) 
+
+/*
+var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++
 }
-
-
+*/
 
 /* 
 console.log('Tudo bem?')
