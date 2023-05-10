057a076e81220aea8c2006aac74bae0f50512c88
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-10 10:56:05 -0300

    Atualizei

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,6 @@
+function contar() {
+    var inicio = document.querySelector('div#txtini')
+    var fim = document.querySelector('div#txtfim')
+    var passo = document.querySelector('div#txtpas')
+    var res = document.querySelector('div#res')
+}
