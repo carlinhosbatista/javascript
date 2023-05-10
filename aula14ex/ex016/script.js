667c583884c8024b70e9ba8997a485a696dd6d9c
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-10 11:48:22 -0300

    Atalizei

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,4 +3,14 @@ function contar() {
     var fim = document.querySelector('div#txtfim')
     var passo = document.querySelector('div#txtpas')
     var res = document.querySelector('div#res')
+    if (inicio.value == 0 || fim.value == 0 || passo.value == 0) {
+        alert('Impossível contar!')    
+    } else if (Number(passo.value) > inicio) {
+        alert(`Passo inválido! Considerando PASSO ${inicio}`)
+    } else {
+        res.innerHTML = 'Contando...'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+    }
 }
