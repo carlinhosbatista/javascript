5b5ef443728ab418e103842d68c11fa0c0122df8
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-10 14:47:12 -0300

    Atualizei

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,16 +1,27 @@
 function contar() {
-    var inicio = document.querySelector('div#txtini')
-    var fim = document.querySelector('div#txtfim')
-    var passo = document.querySelector('div#txtpas')
-    var res = document.querySelector('div#res')
+    let inicio = document.getElementById('txtini')
+    let fim = document.getElementById('txtfim')
+    let passo = document.getElementById('txtpas')
+    let res = document.querySelector('input#res')
     if (inicio.value == 0 || fim.value == 0 || passo.value == 0) {
         alert('Impossível contar!')    
-    } else if (Number(passo.value) > inicio) {
-        alert(`Passo inválido! Considerando PASSO ${inicio}`)
     } else {
-        res.innerHTML = 'Contando...'
+        res.innerHTML = 'Contando: '
         var i = Number(inicio.value)
         var f = Number(fim.value)
-        var p = Number(passo.value)
-    }
+        var p = Number(passo.value) 
+        
+        for(let c = i; c <= f; c++ p)
+    } 
 }
+
+
+
+/*
+else {
+    
+    
+    if(p == 0) {
+        p=1
+    }
+    */
