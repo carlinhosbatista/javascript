83517a4f94302273a19874e5cfbae7e9393d89ae
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-11 09:53:21 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,15 +1,11 @@
 function contar() {
-    let inicio = document.getElementById('txti')
+    let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
+    let pas = document.getElementById('txtp')
     let res = document.getElementById('res')
-    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+    if (ini.value.lenght == 0 || fim.value.lenght == 0 || pas.value.lenght == 0) {
         alert('[ERRO] Digite novamente!')
     } else {
-        
-    }    
+        alert('Tudo ok!')
+    }
 }
-
-//Passo inválido! considerando PASSO 1
-
-
