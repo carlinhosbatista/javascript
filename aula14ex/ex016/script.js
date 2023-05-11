e0c2dbba3b02862267cd5914c9fa3a0f5b741022
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-11 14:45:40 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -3,7 +3,7 @@ function contar() {
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-    if (ini.value == 0 || fim.value == 0 || passo.value == 0) {
+    if (ini.value == 0 || fim.value == 0 || passo.value.lenght == 0) {
         res.innerHTML = 'Impossível contar!'
         //alert('[ERRO] Faltam dados!')
     } else {
@@ -11,7 +11,10 @@ function contar() {
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        
+        if(p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
         if (i < f) {
             // Contagem progressiva
             for(let c = i; c <= f; c += p) {
