a0aeaf30f79c92c16bc7371704a37127ce4df982
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-11 09:25:57 -0300

    ATZ

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,15 @@
+function contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+    if (inicio.value.lenght == 0 || fim.value.lenght == 0 || passo.value.lenght == 0) {
+        alert('[ERRO] Digite novamente!')
+    } else {
+        
+    }    
+}
+
+//Passo inválido! considerando PASSO 1
+
+
