a2cd3e532035e4eb71f71665e9eeab2606109323
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-12 09:13:54 -0300

    Atualizei

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,11 @@
+function tabuada() {
+    let num = document.querySelector('input#txtn')
+    let tab = document.querySelector('select#seltab')
+
+    if (num.value == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)    
+    }
+    
+}
