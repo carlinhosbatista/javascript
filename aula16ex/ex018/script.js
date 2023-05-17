dae93c50d9406ac17da37030026153fe27a33065
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-17 15:50:20 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -2,6 +2,7 @@ let num = document.querySelector('input#fnum')
 let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
+// VALORES GLOBAIS: QUANDO ESTÃO FORA DAS FUNÇÕES
 
 function isNumero(n) { // Descobrir se o número está entre 1 e 100
     if(Number(n) >= 1 && Number(n) <= 100) { // Se o número(n) for >= a 1 e <= a 100
@@ -12,7 +13,7 @@ function isNumero(n) { // Descobrir se o número está entre 1 e 100
 }
 
 function inLista (n, l) {
-    if (l.indexOf(Number(n)) != -1) { // Se na lista o número(n) do índice for diferente de  -1
+    if (l.indexOf(Number(n)) != -1) { // Se na lista o número(n) do índice for diferente de -1
         return true
     } else {
         return false
