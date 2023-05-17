a1e7318514e4c66bd721c5da2ff463875cc57629
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-17 09:42:41 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -3,6 +3,18 @@ let lista = document.querySelector('select#flista')
 let res = document.querySelector('div#res')
 let valores = []
 
+function isNumero(n) {
+    
+}
+
+function inLista (n, l) {
+
+}
+
 function adicionar() {
-    if(isNumero(num.value) && !inlista(num.value))
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+
+    } else {
+        alert('Valor inválido ou já encontrado na lista.')
+    }
 }
