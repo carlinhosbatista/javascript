5e2d6de2094d4b0f7d79aaab07cd3714826a1e6e
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-17 08:49:11 -0300

    Atualizei

---
+++ aula16ex/ex018/script.js
@@ -0,0 +1,8 @@
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function adicionar() {
+    if(isNumero(num.value) && !inlista(num.value))
+}
