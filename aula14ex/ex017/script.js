0df993d5247085ec85e7e8289267e33af227e7a3
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-19 11:34:59 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -7,7 +7,7 @@ function tabuada() {
     } else {
         let n = Number(num.value)
 
-        for(let c = n; c <= 10; c++) {
+        for(let c = 1; c <= 10; c++) {
             let item = document.createElement('option')
             item.text = `${n} x ${c} = ${n*c}`
             tab.appendChild(item)
