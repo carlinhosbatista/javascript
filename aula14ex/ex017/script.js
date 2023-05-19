7947493621e2f11106170193751393ca8721f775
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-19 11:22:27 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,11 +1,16 @@
 function tabuada() {
     let num = document.querySelector('input#txtn')
     let tab = document.querySelector('select#seltab')
-
+    tab.innerHTML = ''
     if (num.value == 0) {
         alert('Por favor, digite um número!')
     } else {
         let n = Number(num.value)
-        let item = document.createe
+
+        for(let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
     }
 }
