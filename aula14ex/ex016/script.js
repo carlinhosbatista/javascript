7377df508ffe8a78032e449b6e1c3347fa79900e
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-21 14:13:48 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -4,5 +4,21 @@ function contar() {
     let passo = document.querySelector('input#txtp')
     let res = document.querySelector('div#res')
 
-    
+    if (ini.value == 0 || fim.value == 0 || passo.value.lenght == 0) {
+        alert('[ERRO] Informe o camp solicitado!')
+    } else {
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        res.innerHTML = 'Contando: <br>'
+        if (p == 0) {
+            alert('Passo incorreto! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            for(c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449} `
+            }
+        }
+    }
 }
