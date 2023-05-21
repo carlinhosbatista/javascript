61175ec3b160643160f391a71a614a2471f636dd
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-21 14:17:33 -0300

    Update script.js

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,29 +1,31 @@
 function contar() {
-    let ini = document.querySelector('input#txti')
-    let fim = document.querySelector('input#txtf')
-    let passo = document.querySelector('input#txtp')
-    let res = document.querySelector('div#res')
-
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
     if (ini.value == 0 || fim.value == 0 || passo.value.lenght == 0) {
         res.innerHTML = 'Impossível contar!'
-        //alert('[ERRO] Informe o camp solicitado!')
+        //alert('[ERRO] Faltam dados!')
     } else {
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        res.innerHTML = 'Contando: <br>'
-        if (p == 0) {
-            alert('Passo incorreto! Considerando PASSO 1')
+        if(p <= 0) {
+            alert('Passo inválido! Considerando PASSO 1')
             p = 1
         }
         if (i < f) {
-            for(c = i; c <= f; c += p) {
-                res.innerHTML += ` ${c} \u{1f449} `
-            }
+            // Contagem progressiva
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }    
         } else {
-            for(c = i; c >= f; c -= p) {
+            // Contagem Regressiva
+            for(let c = i; c >= f; c -= p) {
                 res.innerHTML += ` ${c} \u{1f449}`
             }
         }
+        res.innerHTML += `\u{1f3c1}`
     }
 }
