319ad414f76f7ec23ca3aebc9eef7c6d4873570e
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-21 15:46:26 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -54,7 +54,7 @@ function finalizar() {
         res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}.</p>`
-        res.innerHTML += `<p>Somando todos os valores, temos ${soma}`
-        res.innerHTML += `<p>A média dos valores digitados é ${media}`
+        res.innerHTML += `<p>Somando todos os valores, temos ${soma}.</p>`
+        res.innerHTML += `<p>A média dos valores digitados é ${media}.</p>`
     }
 }
