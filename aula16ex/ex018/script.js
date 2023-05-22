67fd01846e459dff32ce22c16620bdc9f4a1c308
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-22 11:43:47 -0300

    Update script.js

--- aula16ex/ex018/script.js
+++ aula16ex/ex018/script.js
@@ -2,3 +2,31 @@ let num = document.querySelector('input#fnum')
 let list = document.querySelector('input#flista')
 let res = document.querySelector('div#res')
 let val = []
+
+function adicionar() {
+    if (isNumber(num.value) && !inList(num.value, val)) {
+        alert('Tudo ok!')    
+    } else {
+        alert('Valor inválido ou já encontrado na lista!')
+    }
+}
+
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function finalizar() {
+
+}
