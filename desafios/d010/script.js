859b3b627dbea1270541b2089047022377a30b11
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-26 16:59:53 -0300

    Update script.js

--- desafios/d010/script.js
+++ desafios/d010/script.js
@@ -3,10 +3,12 @@ function calc() {
     let b = parseFloat(prompt('Qual é o valor de b?')) 
     let c = parseFloat(prompt('Qual é o valor de c?')) 
     let res = document.querySelector('section#res')
-    let d = (b*2) - 4*a*b 
+    let bval = (b*2)
+    let delta = (4*a*c) - (bval)
+   
     
     res.innerHTML = '<h2>Resolvendo Bhaskara</h2>'
     res.innerHTML += `<p>A equação atual é <strong>${a}x&sup2; + ${b}x + 2 = 0</strong></p>`
     res.innerHTML += `<p>O cálculo realizado será <strong>\u{0394} = ${b}&sup2; - 4 . ${a} . ${c}</strong></p>`
-    res.innerHTML += `O valor calcuado foi <strong>\u{0394} = ${d}`
+    res.innerHTML += `O valor calcuado foi <strong>\u{0394} = ${delta}`
 }
