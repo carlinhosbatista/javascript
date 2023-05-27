337213b0a213dc0014df1e7b03131de3e530f2a6
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-27 18:34:44 -0300

    Create acoes.js

---
+++ exercicios/ex010/acoes.js
@@ -0,0 +1,19 @@
+function acao1() {
+    let res = document.getElementById('saida')
+    res.innerHTML = '<p>Clicou no primeiro botão</p>'
+}
+
+function acao2() {
+    let res = document.getElementById('saida')
+    res.innerHTML += '<p>Clicou no segundoo botão</p>'
+}
+
+function acao3() {
+    let res = document.getElementById('saida')
+    res.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function acao4() {
+    let res = document.getElementById('saida')
+    res.innerHTML += '<p>Clicou no quarto botão</p>'
+}
