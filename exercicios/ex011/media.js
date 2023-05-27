4cb8815b099531d935afe68ab5a02b0442e1538a
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-27 19:48:04 -0300

    Update media.js

--- exercicios/ex011/media.js
+++ exercicios/ex011/media.js
@@ -1,20 +1,20 @@
-function calc() {
-    let nome = prompt('Qual é o nome do aluno?')
-    let n1 = Number(prompt(`Qual foi a primeira nota de ${nome}?`))
-    let n2 = Number(prompt(`Além de ${n1}, qual foi a outra nota de ${nome}?`))
-    let media = (n1 + n2)/2
+function media() {
+    let nom = window.prompt('Qual é o nome do aluno?') 
+    let n1 = Number(window.prompt(`Qual foi a primeira nota de ${nom}?`))
+    let n2 = Number(window.prompt(`Além de ${n1}, qual foi a outra nota de ${nom}?`))
+    med = (n1 + n2)/2 
     
-    let msg = ''
-    if (media >= 6) {
-            msg = 'Meus Parabéns!'
-        } else {
-            msg = 'Estude um pouco mais!'
-        }
+    let msg // cria uma variável e deixa ela vazia
+    if (med >= 6) { // Se por acaso a média foi 6.0 ou mais...
+        msg = 'Meus parabéns!'
+    } else { // senão...
+        msg = 'Estude um pouco mais!'
+    }
+    // O if é uma estrutura que cria uma CONDIÇÃO, que executa um bloco de comandos ou outro, dependendo do resultado de um teste lógico.
 
-    let res = document.querySelector('section#res')
-    res.innerHTML = `<p>Calculando a média final de <mark>${nome}.</p>`
-    res.innerHTML += `<p>As notas obtidas foram <mark>${n1} e ${n2}.</p>`
-    res.innerHTML += `<p>A média final será <mark>${media}</p>`
-    res.innerHTML += `<p>A mensagem que temos é: <strong style='color:red;'>${msg}</strong></p>`
+    let res = document.getElementById('situacao')
+    res.innerHTML = `<p>Calculando a média final de <mark>${nom}</mark>.</p>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${n1} e ${n2}</mark>.</p>` 
+    res.innerHTML += `<p>A média final será <mark>${med}</mark>.</p>`
+    res.innerHTML += `<p>A mensagem que temos é: <strong style='color:red;'>${msg}</strong></p>` // Note que eu usei até um pouco de CSS pra fazer a mensagem ficar vermelha ;)
 }
-
