fe6b650e71264f07ff1bf6e12adaff101ff6b466
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-27 18:39:42 -0300

    Update acoes.js

--- exercicios/ex010/acoes.js
+++ exercicios/ex010/acoes.js
@@ -1,19 +1,17 @@
+let res = document.getElementById('saida')
+
 function acao1() {
-    let res = document.getElementById('saida')
     res.innerHTML = '<p>Clicou no primeiro botão</p>'
 }
 
 function acao2() {
-    let res = document.getElementById('saida')
     res.innerHTML += '<p>Clicou no segundoo botão</p>'
 }
 
 function acao3() {
-    let res = document.getElementById('saida')
     res.innerHTML += '<p>Clicou no terceiro botão</p>'
 }
 
 function acao4() {
-    let res = document.getElementById('saida')
     res.innerHTML += '<p>Clicou no quarto botão</p>'
 }
