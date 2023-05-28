3a9348b2c91004e4314d02e7700984f968e9e079
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-28 16:07:12 -0300

    Update function.js

--- exercicios/ex015/function.js
+++ exercicios/ex015/function.js
@@ -4,8 +4,19 @@ function dateH() {
     let now = new Date
     let exit = document.querySelector('section#exit')
     let day = now.getDate()
+    let month = now.getMonth()
+    let year = now.getFullYear()
+    let dayW = now.getDay()
+    let hour = now.getHours() 
+    let min = now.getMinutes()
+    let sec = now.getSeconds()
 
-    exit.innerHTML = `<p>
-        Dia: <mark>${day}</mark>
-    </p>`
+    exit.innerHTML = `<p>Dia: <mark>${day}</mark></p>`
+    exit.innerHTML += `<p>Mês: <mark>${months[month]}</mark></p>`
+    exit.innerHTML += `<p>Ano: <mark>${year}</mark></p>`
+    exit.innerHTML += `<p>Dia da semana: <mark>${week[dayW]}</mark></p>`
+    exit.innerHTML += `<p>Hora: <mark>${hour}</mark></p>`
+    exit.innerHTML += `<p>Minuto: <mark>${min}</mark></p>`
+    exit.innerHTML += `<p>Segundo: <mark>${sec}</mark></p>`
+   
 }
