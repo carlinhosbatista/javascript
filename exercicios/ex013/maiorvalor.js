76d1b5c0e4053a6785f8e59cbd2887a635cd81a9
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-28 13:41:19 -0300

    Update maiorvalor.js

--- exercicios/ex013/maiorvalor.js
+++ exercicios/ex013/maiorvalor.js
@@ -8,9 +8,10 @@ function bigNumber() {
     let n2 = Number(prompt('Digite outro número:'))
     let big = ''
     if (n1 > n2) {
-        big = n1
+        res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, o maior valor é <strong>${n1}</strong></p>`
+    } else if (n1 < n2) {
+        res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, o maior valor é <strong>${n2}</strong></p>`
     } else {
-        big = n2
+        res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, ambos são <strong>IGUAIS</strong></p>`
     }
-    res.innerHTML = `<p>Analisando os valores <mark>${n1}</mark> e <mark>${n2}</mark>, o maior valor é <strong>${big}</strong></p>`
 }
