68e05655cf199eec17b122686cef4add8eea4cfe
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-29 14:43:46 -0300

    Random numbers

--- exercicios/ex017/function.js
+++ exercicios/ex017/function.js
@@ -1,3 +1,11 @@
-function click() {
-    let gerar = document.querySelector('section#gerar')
+function generate() {
+    let num = Math.floor(100* Math.random())
+    let gen = document.querySelector('section#gen')
+    gerar.innerHTML += `<p>Acabei de pensar no número <mark>${num}!</mark></p>`
+
+}
+
+function clear() {
+    let clear = document.querySelector('button#clear')
+    clear.innerHTML = ''
 }
