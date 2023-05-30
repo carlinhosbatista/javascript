3957818afd392ca325878d0f0a246f3933f35d22
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-30 11:40:23 -0300

    Training a little

--- exercicios/ex018/guess.js
+++ exercicios/ex018/guess.js
@@ -1,24 +1,13 @@
-let res = document.querySelector('section#result')
-let comp = 0
-let player = 0
-
-function rand() {
-    let min = 1
-    let max = 100
-    let dif = max - min
-    let random = Math.random()
-    comp = min + Math.trunc(dif * random)
-}
+let aut = Math.floor(Math.random() * 100 + 1)
 
 function guess() {
-    player = Number(prompt('Qual é o seu palpite?'))
-    
-    if (player == comp) {
-       res.innerHTML += `<p><strong>PARABENS!</strong> Você acertou! Eu tinha sorteado o valor ${comp}</p>`
-
-    } else if (player > comp) {
-        res.innerHTML += `<p>Você falou ${player}. Meu número é <strong>MENOR!</strong></p>`
+    let num = Number(prompt('Qual é o seu palpite?'))
+    let guess = document.querySelector('section#guess')
+    if (num == aut) {
+       guess.innerHTML += `<p><strong>PARABENS!</strong> Você acertou! Eu tinha sorteado o valor ${aut}</p>`
+    } else if (num > aut) {
+        guess.innerHTML += `<p>Você falou ${num}. Meu número é <strong>MENOR!</strong></p>`
     } else {
-        res.innerHTML += `<p>Você falou ${player}. Meu número é <strong>MAIOR!</strong></p>`
+        guess.innerHTML += `<p>Você falou ${num}. Meu número é <strong>MAIOR!</strong></p>`
     }
 }
