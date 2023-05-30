089b8d69322ebfed0bbe292c982fb11b8411b57e
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-30 15:59:09 -0300

    Update function.js

--- exercicios/ex015/function.js
+++ exercicios/ex015/function.js
@@ -2,12 +2,14 @@ function dateH() {
     let months = new Array ('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
     let week = new Array ('Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb')
     let now = new Date
-    let day = now.getDay()
+    let day = now.getDate()
     let month = now.getMonth()
     let year = now.getFullYear()
+    let dayW = now.getDay()
 
     let exit = document.querySelector('section#exit')
-    exit.innerHTML = `<p>Dia: <mark>${week[day]}</mark></p>`
+    exit.innerHTML = `<p>Dia: <mark>${day}</mark></p>`
     exit.innerHTML += `<p>Mês: <mark>${months[month]}</mark></p>`
-    exit.innerHTML += `<p>Ano: ${year}</p>`
+    exit.innerHTML += `<p>Ano: <mark>${year}</mark></p>`
+    exit.innerHTML += `<p>Dia da semana: <mark>${week[dayW]}</mark></p>`
 }
