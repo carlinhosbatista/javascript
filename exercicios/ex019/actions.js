70fc858bb55d6d88e204932bbe009d2748aa4f96
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-31 15:24:46 -0300

    Update actions.js

--- exercicios/ex019/actions.js
+++ exercicios/ex019/actions.js
@@ -1,25 +1,25 @@
 function calc() {
     let n1 = Number(prompt('Primeiro valor:'))
     let n2 = Number(prompt('Segundo valor:'))
-    let op = Number(prompt(`Valores informados ${n1} e ${n2}.\nO que vamos fazer? \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`))
+    let op = Number(prompt(`Valores informados: ${n1} e ${ n2}.\nO que vamos fazer? \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4 Dividir]`))
 
     let exit = document.querySelector('section#result')
     exit.innerHTML = '<h2>Calculando...</h2>'
     switch (op) {
         case 1:
             exit.innerHTML += `<p>${n1} + ${n2} = <strong>${n1+n2}</strong></p>`
-            break
+            break;
         case 2:
             exit.innerHTML += `<p>${n1} - ${n2} = <strong>${n1-n2}</strong></p>`
             break
         case 3:
-            exit.innerHTML += `<p>${n1} x ${n2} = <strong>${n1*n2}</strong></p>`
+            exit.innerHTML += `${n1} x ${n2} = ${n1*n2}`
             break
         case 4:
-            exit.innerHTML += `<p>${n1} / ${n2} = <strong>${(n1/n2).toLocaleString('pt-BR')}</strong></p>`
+            exit.innerHTML += `<p>${n1} / ${n2} = ${(n1/n2).toLocaleString('pt-BR')}</p>`
             break
         default:
-            exit.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles</p>.`
-            break
+            exit.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles.</p>`
+            break;
     }
 }
