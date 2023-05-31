6ab2a5c0e50ef7ff08207fc1c20b579411c86ea1
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-31 10:51:46 -0300

    Atz

--- exercicios/ex019/actions.js
+++ exercicios/ex019/actions.js
@@ -2,4 +2,7 @@ function calc() {
     let n1 = Number(prompt('Primeiro valor:'))
     let n2 = Number(prompt('Segundo valor:'))
     let op = Number(prompt(`Valores informados ${n1} e ${n2}.\nO que vamos fazer? \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`))
+
+    let exit = document.querySelector('section#result')
+    exit.innerHTML = '<h2>Calculando</h2>'    
 }
