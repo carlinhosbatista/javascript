d3f78d8c78b5bfd67c20b754cbbbd9b152687aeb
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-31 10:41:34 -0300

    Update actions.js

--- exercicios/ex019/actions.js
+++ exercicios/ex019/actions.js
@@ -1,31 +1,5 @@
-
 function calc() {
     let n1 = Number(prompt('Primeiro valor:'))
     let n2 = Number(prompt('Segundo valor:'))
-    let op = prompt(`Valores informados: ${n1} e ${n2}. \n O que vamos fazer? \n [1] Somar \n [2] Subtrair \n [3] Multiplicar \n [4] Dividir
-    `)
-
-    let res = document.querySelector('section#result')
-    res.innerHTML = '<h2>Calculando</h2>'
-    switch (op) {
-        case 1:
-            res.innerHTML += `${n1} + ${n2} = ${n1 + n2}`
-            break
-        case 2:
-            res.innerHTML += `${n1} + ${n2} = ${n1-n2}`
-            break
-        case 3:
-            res.innerHTML += `${n1} * ${n2} = ${n1*n2}`
-            break
-        case 4:
-            res.innerHTML += `${n1} / ${n2} = ${n1/n2}`
-            break
-        default:
-            saida.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles.</p>`
-            break
-
-    }
-    
-
-    
+    let op = Number(prompt(`Valores informados ${n1} e ${n2}.\nO que vamos fazer? \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`))
 }
