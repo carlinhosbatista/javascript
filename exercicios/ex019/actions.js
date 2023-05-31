8a50cb9c630bbd65214eeac33fd475fe2a6f4f58
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-31 10:31:32 -0300

    Atz

--- exercicios/ex019/actions.js
+++ exercicios/ex019/actions.js
@@ -1,27 +1,27 @@
 
-function go() {
-    let val1 = Number(prompt('Primeiro valor:'))
-    let val2 = Number(prompt('Segundo valor:'))
-    let op = prompt(`Valores informados: ${val1} e ${val2}. \n O que vamos fazer? \n [1] Somar \n [2] Subtrair \n [3] Multiplicar \n [4] Dividir
+function calc() {
+    let n1 = Number(prompt('Primeiro valor:'))
+    let n2 = Number(prompt('Segundo valor:'))
+    let op = prompt(`Valores informados: ${n1} e ${n2}. \n O que vamos fazer? \n [1] Somar \n [2] Subtrair \n [3] Multiplicar \n [4] Dividir
     `)
 
     let res = document.querySelector('section#result')
     res.innerHTML = '<h2>Calculando</h2>'
     switch (op) {
         case 1:
-            res.innerHTML += `${val1} + ${val2} = ${}`
+            res.innerHTML += `${n1} + ${n2} = ${n1 + n2}`
             break
         case 2:
-            res.innerHTML = 
+            res.innerHTML += `${n1} + ${n2} = ${n1-n2}`
             break
         case 3:
-            res.innerHTML = 
+            res.innerHTML += `${n1} * ${n2} = ${n1*n2}`
             break
         case 4:
-            res.innerHTML = 
+            res.innerHTML += `${n1} / ${n2} = ${n1/n2}`
             break
         default:
-            res.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${val1} e ${val2}, mas não sei o que fazer com eles.</p>`
+            saida.innerHTML += `<p>OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles.</p>`
             break
 
     }
