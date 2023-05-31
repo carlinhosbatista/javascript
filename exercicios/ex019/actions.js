7e6cadf414b1036bc0c8f1747691c04e318293f6
Carlos Batista <carlosbatistadev7@gmail.com>
2023-05-31 10:55:20 -0300

    Update actions.js

--- exercicios/ex019/actions.js
+++ exercicios/ex019/actions.js
@@ -4,5 +4,14 @@ function calc() {
     let op = Number(prompt(`Valores informados ${n1} e ${n2}.\nO que vamos fazer? \n[1] Somar \n[2] Subtrair \n[3] Multiplicar \n[4] Dividir`))
 
     let exit = document.querySelector('section#result')
-    exit.innerHTML = '<h2>Calculando</h2>'    
+    exit.innerHTML = '<h2>Calculando...</h2>'
+    switch (op) {
+        case 1:
+            exit.innerHTML += `${n1} + ${n2} = ${n1+n2}`
+            break;
+    
+        default:
+            exit.innerHTML += `OPÇÃO INVÁLIDA! Você digitou ${n1} e ${n2}, mas não sei o que fazer com eles.`
+            break;
+    }
 }
