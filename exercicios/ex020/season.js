87f9ccfbaac25f74ace38b4743e58148158bfc79
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-01 10:17:00 -0300

    Update season.js

--- exercicios/ex020/season.js
+++ exercicios/ex020/season.js
@@ -1,14 +1,24 @@
 function season() {
-    let month = Number(prompt('Digite o mês por extenso (ex: Setembro'))
-
+    let month = prompt('Digite o mês por extenso (ex: Setembro')
     let exit = document.getElementById('exit')
+    let season = ''
     switch (month) {
-        case 1:
-            if (month == 'Janeiro' || month == 'Fevereiro' || month == 'Março')
-                exit.innerHTML = `<P>No mês de ${month}, estamos na estação INVERNO.</P>`
-            break;
-    
+        case 'Janeiro': case 'Fevereiro': case 'Março':
+            season = 'INVERNO'
+            break
+        case 'Abril': case 'Maio': case 'Junho':
+            season = 'PRIMAVERA'
+            break
+        case 'Julho': case 'Agosto': case 'Setembro':
+            season = 'VERÃO'
+            break
+        case 'Outubro': case 'Novembro': case 'Dezembro':
+            season = 'OUTONO'
+            break
         default:
-            break;
+            season = 'INDEFINIDA'
+            break
+
     }
+    exit.innerHTML = `<P>No mês de <mark>${month}</mark>, estamos na estação <strong><mark>${season}</mark></strong>.</P>`
 }
