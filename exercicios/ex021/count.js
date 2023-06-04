2f6c3f87837b4769d0b9baad8e3425b6b03a71c4
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-04 14:55:21 -0300

    Training

--- exercicios/ex021/count.js
+++ exercicios/ex021/count.js
@@ -1,3 +1,10 @@
 function count() {
-    
+    let exit.document.getElementById('exit')
+    let exit = document.getElementById('exit').style.fontSize = '20px'
+    let c = 1
+    exit.innerHTML = '<h2>Contando de 1 até 10</h2>'
+    while (c <= 10) {
+            exit.innerHTML += ` ${c} \u{1f449}`
+        c++
+    }
 }
