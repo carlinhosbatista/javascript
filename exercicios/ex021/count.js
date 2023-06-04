d6e2e20ad8439e1d1ee7b7a9c30822d057421761
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-03 23:15:58 -0300

    Update count.js

--- exercicios/ex021/count.js
+++ exercicios/ex021/count.js
@@ -1,4 +1,8 @@
 function count() {
     let exit = document.getElementById('exit')
-    for(let c = 1; c <= 10; c++)
+    for(let c = 1; c <= 10; c++) {
+        exit.innerHTML += 'Contando de 1 até 10'
+        document.body.style.fontSize = '2em'
+    }
+    exit.innerHTML += `${c}`
 }
