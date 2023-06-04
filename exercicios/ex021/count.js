796c5a25b42951c84f7c6b3770414fcc91b63e05
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-03 23:08:53 -0300

    Testing

--- exercicios/ex021/count.js
+++
@@ -1,9 +0,0 @@
-function count() {
-    let exit = document.querySelector('section#exit')
-    exit.innerHTML += '<p><h2>Contando de 1 até 10</h2></p>'
-    for(c = 1; c < 11; c++) {
-        exit.innerHTML += ` ${c} \u{1f449}`
-    }
-    exit.innerHTML += `\u{1f3c1}`
-    document.body.style.fontSize = '1.2em'
-}
