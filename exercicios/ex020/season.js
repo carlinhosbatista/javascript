9eaee6ab5db4a5e1a39ea97dc6d52df4bc03e6b9
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-03 22:53:28 -0300

    Update season.js

--- exercicios/ex020/season.js
+++ exercicios/ex020/season.js
@@ -1,3 +1,15 @@
 function season() {
-    
+    let season = prompt('Digite o mês em extenso (ex: Setembro')
+    let exit = document.querySelector('section#exit')
+    switch (season.toUpperCase()) {
+        case 'JANEIRO': case 'FEVEREIRO': case 'MARÇO':
+            season = 'INVERNO'
+            break;
+        case 'ABRIL': case 'MAIO': case 'JUNHO':
+            season = 'PRIMAVERA'
+            break
+        case 'JULHO': case 'AGOSTO': case 'OUTUBRO'
+        default:
+            break;
+    }
 }
