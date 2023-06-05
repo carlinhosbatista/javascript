66b87678b4bb5bc266460405aeac1a5e6be3b8ae
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-05 11:01:56 -0300

    Update script.js

--- exercicios/ex024/script.js
+++ exercicios/ex024/script.js
@@ -1,9 +1,10 @@
 function count() {
     let exit = document.querySelector('p#exit')
     let c = 10
-    exit.innerHTML += '<h2>Contagem regressiva de 10 a 1</h2>'
+    exit.innerHTML += '<h2>Contagem Regressiva de 10 a 1</h2>'
     do {
         exit.innerHTML += ` ${c} \u{1f449}`
         c -= 1
     } while (c >= 1)
+    exit.innerHTML += ` \u{1f3c1}`
 }
