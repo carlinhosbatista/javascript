883e2922bee77d62fef6dd8622a3ccfe85922ec3
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-06 09:30:09 -0300

    Training

--- exercicios/ex025/script.js
+++ exercicios/ex025/script.js
@@ -1,10 +1,9 @@
 function count() {
-    let num = document.getElementById('num')
+    let num = Number(document.getElementById('num').value) 
     let exit = document.getElementById('exit')
-    let n = Number(num.value)
 
-    exit.innerHTML += `<h2>Contando de 0 até ${n}</h2>`
-    for(c = 0; c <= n; c++) {
+    exit.innerHTML += `<h2>Contando de 0 até ${num}</h2>`
+    for(c = 0; c <= num; c++) {
         exit.innerHTML += ` ${c} \u{1f449}`
     }
     exit.innerHTML += ` \u{1f3c1}`
