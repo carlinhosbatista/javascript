f56558e96b3bccfc106d99b5ba900b85f239e708
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-07 14:16:17 -0300

    Atualizei

--- exercicios/ex028/script.js
+++ exercicios/ex028/script.js
@@ -1,12 +1,3 @@
-function fatorial() {
-    let n = Number(document.querySelector('input#num').value)
-    let exit = document.querySelector('p#exit')
-    let fat = 1
-    exit.innerHTML += `<h2>Calculando ${n}!</h2>`
-   for(let c = n; c > 1; c--)  {
-        exit.innerHTML += `${c} x `
-        fat *= c
-   }
-   exit.innerHTML += `=${fat}`
+function fatorial(n) {
+    
 }
-
