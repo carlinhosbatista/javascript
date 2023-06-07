f025cfb3a0ae5c579d7c22159a0e805897db963d
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-07 10:24:22 -0300

    Training

--- exercicios/ex028/script.js
+++ exercicios/ex028/script.js
@@ -1,4 +1,6 @@
 function fatorial() {
-    let n = Number(document.querySelector('input#num'))
+    let n = Number(document.querySelector('input#num').value)
     let exit = document.querySelector('p#exit')
+    exit.innerHTML += `<h2>Calculando ${n}!</h2>`
+    
 }
