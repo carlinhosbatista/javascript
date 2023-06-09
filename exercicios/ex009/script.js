53999d2cdfef01942a1b32fbc95a31cda9e5c0ac
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-09 14:05:42 -0300

    Update script.js

--- exercicios/ex009/script.js
+++ exercicios/ex009/script.js
@@ -1,11 +1,11 @@
 let c = 0
-let res = document.querySelector('section#exit')
+let res = document.getElementById('exit')
+
 function clicou() {
     c++
-    res.innerHTML = `<p>O contador está com <mark>${c}</mark> cliques</p>`
+    res.innerHTML = `<p>O contador está com <mark>${c}</mark> cliques.</p>`
 }
 
 function finalizar() {
-    res.innerHTML = `${c = ''}`
-    
+
 }
