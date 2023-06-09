ff9fd3770050e601298eb329f18e018c828710ea
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-09 14:10:17 -0300

    Just training

--- exercicios/ex010/acoes.js
+++ exercicios/ex010/acoes.js
@@ -1,17 +1,4 @@
-let res = document.getElementById('saida')
-
+let 
 function acao1() {
-    res.innerHTML += '<p>Clicou no primeiro botão</p>'
-}
 
-function acao2() {
-    res.innerHTML += '<p>Clicou no segundo botão</p>'
-}
-
-function acao3() {
-    res.innerHTML += '<p>Clicou no terceiro botão</p>'
-}
-
-function acao4() {
-    res.innerHTML += '<p>Clicou no quarto botão</p>'
 }
