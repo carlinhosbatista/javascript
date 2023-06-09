6e8335db14332cb02532c906c3dd72ae15ffc6d1
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-09 14:12:11 -0300

    Update acoes.js

--- exercicios/ex010/acoes.js
+++ exercicios/ex010/acoes.js
@@ -1,4 +1,8 @@
-let 
+let exit = document.querySelector('section#exit')
 function acao1() {
+    exit.innerHTML += 'Clicou no primeiro botão.'
+}
 
+function acao2() {
+    exit.innerHTML += 'Clicou no segundo botão.'
 }
