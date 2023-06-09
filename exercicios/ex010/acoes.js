d9fb7da21e8199fd0a9cdf8567d8999ad3e51cba
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-09 14:14:02 -0300

    Update acoes.js

--- exercicios/ex010/acoes.js
+++ exercicios/ex010/acoes.js
@@ -1,16 +1,16 @@
 let exit = document.querySelector('section#exit')
 function acao1() {
-    exit.innerHTML += 'Clicou no primeiro botão.'
+    exit.innerHTML += '<p>Clicou no primeiro botão.</p>'
 }
 
 function acao2() {
-    exit.innerHTML += 'Clicou no segundo botão.'
+    exit.innerHTML += '<p>Clicou no segundo botão.</p>'
 }
 
 function acao3() {
-    exit.innerHTML += 'Clicou no terceiro botão.'
+    exit.innerHTML += '<p>Clicou no terceiro botão.</p>'
 }
 
 function acao4() {
-    exit.innerHTML += 'Clicou no quarto botão.'
+    exit.innerHTML += '<p>Clicou no quarto botão.</p>'
 }
