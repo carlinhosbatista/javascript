382635ad19440e7514dd0088791092b856863cdb
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-10 18:59:35 -0300

    Lets go

---
+++ exercicios/ex011/media.js
@@ -0,0 +1,7 @@
+function calc() {
+    let nome = prompt('Qual é o nome do aluno?')
+    let n1 = Number(prompt(`Qual foi a primeira nota de ${nome}?`))
+    let n2 = Number(prompt(`Além de ${n1}, qual foi a outra nota de ${nome}?`))
+    let res = document.getElementById('exit')
+
+}
