e5058433fbe2f9bbf943fbb182552c07375ec33f
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-10 19:21:56 -0300

    Training

--- exercicios/ex012/parimpar.js
+++ exercicios/ex012/parimpar.js
@@ -1,12 +1,11 @@
 function test() {
-    let n = Number(prompt('Digite um número:'))
-    let tipo = ''
-    if (n % 2 == 0) {
-        tipo = 'PAR'
+    let n = prompt('Diga um número:')
+    let res = document.querySelector('section#exit')
+    let resp = ''
+    if (n%2 == 0) {
+        resp = "PAR!"
     } else {
-        tipo = 'ÍMPAR'
+        resp = 'ÍMPAR!'
     }
-
-    let res = document.querySelector('section#res')
-    res.innerHTML = `<p>O número ${n} que foi digitado é <strong font-size: 1.3em;>${tipo}!</strong></p>`
+    res.innerHTML = `<p>O número ${n} que foi digitado é ${resp}</p>`
 }
