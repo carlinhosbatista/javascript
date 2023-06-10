4a42f04e6b49bf9604074e8fc759635392d1a10a
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-10 19:02:34 -0300

    Update media.js

--- exercicios/ex011/media.js
+++ exercicios/ex011/media.js
@@ -4,4 +4,7 @@ function calc() {
     let n2 = Number(prompt(`Além de ${n1}, qual foi a outra nota de ${nome}?`))
     let res = document.getElementById('exit')
 
+    res.innerHTML += `<p>Calculando a média final de <mark>${nome}</mark>.</p>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${n1}</mark> e <mark>${n2}</mark>.</p>`
+
 }
