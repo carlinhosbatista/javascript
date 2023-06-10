389d79583c4efa22583390c9f0095601d6aa7c3a
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-10 20:06:23 -0300

    Trying again

--- exercicios/ex013/maiorvalor.js
+++ exercicios/ex013/maiorvalor.js
@@ -1,1 +1,13 @@
-12345
+function val() {
+    let n1 = Number(prompt('Digite um número:'))
+    let n2 = Number(prompt('Digite outro valor:'))
+
+    let res = document.querySelector('section#exit')
+    if (n1 > n2) {
+        res.innerHTML = `Analisando os valores ${n1} e ${n2}, o maior valor é ${n1}`
+    } else if (n1 < n2) {
+        res.innerHTML = `Analisando os valores ${n1} e ${n2}, o maior valor é ${n2}`
+    } else {
+        res.innerHTML = `<p>Analisando os valores ${n1} e ${n2}, ambos são <strong>IGUAIS</strong></p>`
+    }
+}
