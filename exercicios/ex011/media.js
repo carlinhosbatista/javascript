9d0577cbc99bcc7a94d9f974e05ada5a8f3716d7
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-10 19:13:33 -0300

    Update media.js

--- exercicios/ex011/media.js
+++ exercicios/ex011/media.js
@@ -2,14 +2,19 @@ function calc() {
     let nome = prompt('Qual é o nome do aluno?')
     let n1 = Number(prompt(`Qual foi a primeira nota de ${nome}?`))
     let n2 = Number(prompt(`Além de ${n1}, qual foi a outra nota de ${nome}?`))
-    let res = document.getElementById('exit')
-    media = (n1+n2)/2
-    res.innerHTML += `<p>Calculando a média final de <mark>${nome}</mark>.</p>`
-    res.innerHTML += `<p>As notas obtidas foram <mark>${n1}</mark> e <mark>${n2}</mark>.</p>`
-    res.innerHTML += `<p>A média final será <mark>${media}</mark></p>`
-    if(media < 6) {
-        res.innerHTML += `A mensagem que temos é: Estude um pouco mais!`
-    } else {
-        res.innerHTML += `<p>A mensagem que temos é: Meus parabéns!</p>`
-    }
+    let media = (n1 + n2)/2
+    
+    let msg = ''
+    if (media >= 6) {
+            msg = 'Meus Parabéns!'
+        } else {
+            msg = 'Estude um pouco mais!'
+        }
+
+    let res = document.querySelector('section#exit')
+    res.innerHTML = `<p>Calculando a média final de <mark>${nome}.</p>`
+    res.innerHTML += `<p>As notas obtidas foram <mark>${n1} e ${n2}.</p>`
+    res.innerHTML += `<p>A média final será <mark>${media}</p>`
+    res.innerHTML += `<p>A mensagem que temos é: <strong style='color:red;'>${msg}</strong></p>`
 }
+
