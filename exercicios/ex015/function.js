72997047e51f77212739c1a3711daa0b5072b5da
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-11 14:46:49 -0300

    Update function.js

--- exercicios/ex015/function.js
+++ exercicios/ex015/function.js
@@ -2,10 +2,14 @@ function sistem() {
     let now = new Date()
     let week = new Array('Dom', ' Seg', 'Ter', 'Qua', 'Qui', ' Sex', 'Sáb')
     let months = new Array('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
-    let day = now.getDate()
+    let date = now.getDate()
     let month = now.getMonth()
-    let exit = document.querySelector('section#exit')
+    let year = now.getFullYear()
+    let day = now.getDay()
 
-    exit.innerHTML += `<p>Dia: <mark>${day}</mark></p>`
+    let exit = document.querySelector('section#exit')
+    exit.innerHTML += `<p>Dia: <mark>${date}</mark></p>`
     exit.innerHTML += `<p>Mês: <mark>${months[month]}</mark></p>`
+    exit.innerHTML += `<p>Ano: <mark>${year}</mark></p>`
+    exit.innerHTML += `<p>Dia da semana: <mark>${week[day]}</mark></p>`
 }
