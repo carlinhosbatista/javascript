64071b4e0896a7b66428bea25450ab1ca58d4659
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-12 10:34:40 -0300

    Update guess.js

--- exercicios/ex018/guess.js
+++ exercicios/ex018/guess.js
@@ -1,12 +1,19 @@
+let shot = document.querySelector('section#guess')
+
 function rand() {
     let min = 1
     let max = 100
     let dif = max - min
     let comp = Math.random()
     let n = min + Math.trunc(dif * comp)
+
+    
 }
 
-let guess = document.querySelector('section#guess')
+
 function guess() {
     let player = Number(prompt('Qual é o seu palpite?'))
+    if(player == comp) {
+        shot.innerHTML += `Parabéns! Você acertou! Eu tinha sorteado o valor ${n}`
+    }
 }
