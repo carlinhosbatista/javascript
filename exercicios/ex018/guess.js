e748eab04333369d16e157f991cbe0cc34ec14a4
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-12 14:45:18 -0300

    Update guess.js

--- exercicios/ex018/guess.js
+++ exercicios/ex018/guess.js
@@ -1,14 +1,6 @@
-let aut = Math.floor(Math.random() * 100 + 1)
+let comp = Math.floor(Math.random() * 100 + 1)
 
 function guess() {
-    let num = Number(prompt('Qual é o seu palpite?'))
+    
     let guess = document.querySelector('section#guess')
-    if (num == aut) {
-       guess.innerHTML += `<p><strong>PARABENS!</strong> Você acertou! Eu tinha sorteado o valor ${aut}</p>`
-       document.querySelector('button#bt').style.visibility = 'hidden'
-    } else if (num > aut) {
-        guess.innerHTML += `<p>Você falou ${num}. Meu número é <strong>MENOR!</strong></p>`
-    } else {
-        guess.innerHTML += `<p>Você falou ${num}. Meu número é <strong>MAIOR!</strong></p>`
-    }
 }
