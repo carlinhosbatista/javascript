4cfe7cc2aa0ba76bbb57a022b6e51c288e31afe5
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-12 14:54:55 -0300

    Update guess.js

--- exercicios/ex018/guess.js
+++ exercicios/ex018/guess.js
@@ -1,6 +1,12 @@
 let comp = Math.floor(Math.random() * 100 + 1)
 
 function guess() {
-    
+    let player = Number(prompt('Qual é o seu palpite?'))
     let guess = document.querySelector('section#guess')
+
+    if (player > comp) {
+        guess.innerHTML = `<p>Você falou ${player}. O meu n´mero é <strong>MENOR!</strong></p>`
+    } else if (player < comp) {
+        guess.innerHTML += `<p>Você falou ${player}. Meu número é <strong>MAIOR!</strong></p>`
+    }
 }
