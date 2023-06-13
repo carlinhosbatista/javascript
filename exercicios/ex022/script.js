9321928f1178f01c3ad27fe9d454744581992cac
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-13 14:15:39 -0300

    Yeah

--- exercicios/ex022/script.js
+++ exercicios/ex022/script.js
@@ -1,15 +1,12 @@
 function count() {
-    let exit = document.querySelector('p#exit')
-    let c = 1
+    let exit = document.getElementById('exit')
     exit.innerHTML += '<h2>Contando de 1 até 10, marcando os pares</h2>'
-    while (c <= 10) {
-        if (c%2 == 0)     {
-            exit.innerHTML += ` ${c} \u{1f449}`
-            c++
+    
+    for(c = 1; c <= 10; c++) {
+        if (c%2 == 0) {
+            exit.innerHTML += ` <strong>${c}</strong> \u{1f449}`
         } else {
             exit.innerHTML += ` ${c} \u{1f449}`
-            c++
         }
     }
-    exit.innerHTML += ` \u{1f3c1}`
 }
