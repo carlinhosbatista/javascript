aa2f44c98a170aea9f29e52b8e3b13054936ed05
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-13 14:27:04 -0300

    Get up

--- exercicios/ex023/script.js
+++ exercicios/ex023/script.js
@@ -1,10 +1,11 @@
 function count() {
-    let exit = document.getElementById('exit')
-    let c = 2
+    let exit = document.querySelector('p#exit')
     exit.innerHTML += '<h2>Números pares de 1 até 10</h2>'
-    do {
-        exit.innerHTML += ` ${c} \u{1f449}`
-        c += 2
-    } while (c <= 10)
+
+    for(c = 2; c <= 10; c++) {
+        if (c%2 == 0) {
+            exit.innerHTML += ` ${c} \u{1f449}`
+        }
+    }
     exit.innerHTML += ` \u{1f3c1}`
 }
