a93f64dadb9b42ddae935b17df03590d32a17e87
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-14 11:00:20 -0300

    Update script.js

--- exercicios/ex027/script.js
+++ exercicios/ex027/script.js
@@ -1,9 +1,8 @@
 function tab() {
     let n = Number(document.querySelector('input#num').value)
     let exit = document.querySelector('p#exit')
-
-    exit.innerHTML += `<h2>Tabuada de ${n}</h2>`
+    exit.innerHTML = `<h2>Tabuada de ${n}</h2>`
     for(c = 1; c <= 10; c++) {
-        exit.innerHTML += `${n} x ${c} = <strong>${n*c}</strong> <br>`
+        exit.innerHTML += `${n} x ${c} = <strong>${n * c}</strong><br>`
     }
 }
