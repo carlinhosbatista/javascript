1e4535ea0e9c354fe3c78bf85aa7ee96a1afe1f3
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-14 10:42:15 -0300

    Update script.js

--- exercicios/ex026/script.js
+++ exercicios/ex026/script.js
@@ -1,16 +1,17 @@
 function count() {
-    let n1 = Number(document.querySelector('input#n1').value)
-    let n2 = Number(document.querySelector('input#n2').value)
-    let exit = document.querySelector('p#exit')
+    let n1 = Number(document.getElementById('n1').value)
+    let n2 = Number(document.getElementById('n2').value) 
+    let exit = document.getElementById('exit')
+
     exit.innerHTML += `<h2>Contando de ${n1} até ${n2}</h2>`
-    if(n1 < n2) {
-        for (c = n1; c <= n2; c++) {
-            exit.innerHTML += `${c} \u{1f449}`
+    if (n1 < n2) {
+        for(c = n1; c <= n2; c++) {
+            exit.innerHTML += ` ${c} \u{1f449}`
         }
     } else {
         for(c = n1; c >= n2; c--) {
-            exit.innerHTML += `${c} \u{1f449}`
+            exit.innerHTML += ` ${c} \u{1f449}`
         }
     }
-    exit.innerHTML += ` \u{1f3c1}`
+    exit.innerHTML ` \u{1f3c1}`
 }
