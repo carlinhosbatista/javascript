023d3ad15b0c233cd6040edd1fcf728a6a25767e
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-15 14:34:00 -0300

    Atualizei

--- desafios/d005/script.js
+++ desafios/d005/script.js
@@ -10,10 +10,10 @@ function converter() {
     let mm = dis*1000
 
     res.innerHTML = `<h2>A distância de ${dist} metros, corresponde a...</h2>`
-    res.innerHTML += `<p>${km.toFixed(3).toString().replace(".", ",")} quilômetros (Km)</p>`
-    res.innerHTML += `<p>${hm.toFixed(3).toString().replace(".", ",")} hectômetros (Hm)</p>`
-    res.innerHTML += `<p>${dam.toFixed(3).toString().replace(".", ",")} decâmetros (Dam)</p>`
-    res.innerHTML += `<p>${dm.toLocaleString('pt-BR')} decímetro (dm)</p>`
-    res.innerHTML += `<p>${cm.toLocaleString('pt-BR')} centímetro (cm)</p>`
+    res.innerHTML += `<p><strong>${km.toFixed(3).toString().replace(".", ",")} quilômetros (Km)</strong></p>`
+    res.innerHTML += `<p><strong>${hm.toFixed(3).toString().replace(".", ",")} hectômetros (Hm)</strong></p>`
+    res.innerHTML += `<p><strong>${dam.toFixed(3).toString().replace(".", ",")} decâmetros (Dam)</strong></p>`
+    res.innerHTML += `<p><strong>${dm.toLocaleString('pt-BR')} decímetro (dm)</strong></p>`
+    res.innerHTML += `<p><strong>${cm.toLocaleString('pt-BR')} centímetro (cm)</strong></p>`
     res.innerHTML += `<p><strong>${mm.toLocaleString('pt-BR')} milímetro (mm)</strong></p>`
 }
