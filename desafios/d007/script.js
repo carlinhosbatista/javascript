6964f8c44e5d3c66761163df54d1056360645052
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-19 15:34:53 -0300

    Update script.js

--- desafios/d007/script.js
+++ desafios/d007/script.js
@@ -1,9 +1,8 @@
-let cot = prompt('Antes de mais nada. Quanto está a cotação do dolár agora?') // Cotação
-    function convert() {
-        let real = prompt('Quantos R$ você tem na carteira?')
-        let reais = parseFloat(real).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'}) //Conversão para moeda corrente
-        let rdolar = real/cot
-        let res = document.querySelector('section#res')
+let cotD = parseFloat(prompt('Antes de mais nada. Quanto está a cotação do dólar agora?'))
+let res = document.getElementById('res')
 
-        res.innerHTML = `<p>Com ${reais} que você tem e a cotação de hoje, você vai conseguir comprar ${rdolar.toLocaleString('en-US', {style: 'currency', currency: 'USD'})}.</p>`
-    }
+function convert() {
+    let reais = parseFloat(prompt('Quantos R$ você tem na carteira?'))
+    let dol = reais/cotD
+    res.innerHTML += `<p>Com ${reais.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}, você vai conseguir comprar ${dol.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}.</p>`
+}
