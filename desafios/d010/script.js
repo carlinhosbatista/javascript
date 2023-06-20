e5b68ce0d8e2c88edd26d8b5febc4519e3829b55
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-20 12:10:50 -0300

    Keep working

--- desafios/d010/script.js
+++ desafios/d010/script.js
@@ -1,12 +1,12 @@
 function calc() {
-    let a = parseFloat(prompt('Qual é o valor de a?')) 
-    let b = parseFloat(prompt('Qual é o valor de b?')) 
-    let c = parseFloat(prompt('Qual é o valor de c?')) 
-    let res = document.querySelector('section#res')
-    let delta = (b*2)-(4*a*c)
+    let a = Number(prompt('Qual é o valor de a?'))
+    let b = Number(prompt('Qual é o valor de b?'))
+    let c = Number(prompt('Qual é o valor de c?'))
+    let res = document.querySelector('p#res')
 
+    let delta = Math.pow(b, 2) - 4 * a * c
     res.innerHTML = '<h2>Resolvendo Bhaskara</h2>'
-    res.innerHTML += `<p>A equação atual é <strong>${a}x&sup2; + ${b}x + 2 = 0</strong></p>`
-    res.innerHTML += `<p>O cálculo realizado será <strong>\u{0394} = ${b}&sup2; - 4 . ${a} . ${c}</strong></p>`
-    res.innerHTML += `O valor calculado foi <strong><mark>\u{0394} = ${Math.abs(delta)}`
+    res.innerHTML += `<p>A equação atual é <strong>${a}x<sup>2</sup> + ${b}x + ${c} = 0</strong></p>`
+    res.innerHTML += `<p>O cálculo realizado será <strong>\u{0394} = ${b}<sup>2</sup> - 4 . ${a} . ${c}</strong></p>`
+    res.innerHTML += `<p>O valor calculado foi <mark><strong>\u{0394} = ${delta}</mark></strong></p>`
 }
