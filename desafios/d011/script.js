4e256acf21af642e23fced294537102526c3f42c
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-20 16:32:29 -0300

    Training

---
+++ desafios/d011/script.js
@@ -0,0 +1,12 @@
+function leapY() {
+    let year = Number(prompt('Qual é o ano que você quer verificar?'))
+    let exit = document.querySelector('p#exit')
+    let leapYear = ''
+    exit.innerHTML = `<h2>Analisando o ano de ${year}...</h2>`
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0)) {
+        leapYear = `<mark style = "background-color: lightgreen; color: green;"> É BISSEXTO</mark> \u{2705}`
+    } else {
+        leapYear = `<mark style = "background-color: #f18f8f; color: darkred;"> NÃO É BISSEXTO</mark> \u{274C}`
+    }
+    exit.innerHTML += `O ano de ${year} ${leapYear}`
+}
