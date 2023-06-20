b0b31f80a9a395892e5cfebc6fd73de86ffe4688
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-20 10:42:12 -0300

    Training

--- desafios/d009/script.js
+++ desafios/d009/script.js
@@ -1,13 +1,12 @@
 function reajuste() {
-    let nome = prompt('Qual é o nome do funcionário?')
-    let sal = parseFloat(prompt(`Qual é o salário de ${nome}`))  
-    let porc = prompt(`O salário de ${nome} vai ser reajustado em qual porcentagem?`)
-    let aumento = sal*porc/100
-    let nSal = parseFloat(sal) + parseFloat(aumento)  
-    let res = document.querySelector('section#res')
+    let name = prompt('Qual é o nome do funcionário?')
+    let sal = parseFloat(prompt(`Qual é o salário de ${name}?`))
+    let perc = parseFloat(prompt(`O salário de ${name} vai ser reajustado em qual porcentagem?`))
+    let res = document.querySelector('p#res')
 
-    res.innerHTML = `<p><h2>${nome} recebeu um aumento salarial!</h2></p>`
-    res.innerHTML += `<p>O salário atual era ${sal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}.</p>`
-    res.innerHTML += `<p>Com um aumento de ${porc}%, o salário vai aumentar ${aumento.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})} no próximo mês.</p>`
-    res.innerHTML += `E a partir daí, ${nome} vai passar a ganhar ${nSal.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}.`
+    let reajuste = sal*perc/100
+    res.innerHTML = `<h2>${name} recebeu um aumento salarial!</h2>`
+    res.innerHTML += `<p>O salário atual é ${sal.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>`
+    res.innerHTML += `<p>Com um aumento de ${perc}%, o salário vai aumentar ${reajuste.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })} no próximo mês.</p>`
+    res.innerHTML += `<p>E a partir daí, ${name} vai passar a ganhar ${(sal+reajuste).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}.</p>`
 }
