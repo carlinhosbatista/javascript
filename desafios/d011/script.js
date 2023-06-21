aa7d00ebd10e5913aa014f2837ac9e809198123b
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-21 11:21:11 -0300

    Update script.js

--- desafios/d011/script.js
+++ desafios/d011/script.js
@@ -4,9 +4,9 @@ function leapY() {
     let leapYear = ''
     exit.innerHTML = `<h2>Analisando o ano de ${year}...</h2>`
     if ((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0)) {
-        leapYear = `<mark style = "background-color: lightgreen; color: green;"> É BISSEXTO</mark> \u{2705}`
+        leapYear = `<strong style = "background-color: lightgreen; color: green;"> É BISSEXTO</strong> \u{2705}`
     } else {
-        leapYear = `<mark style = "background-color: #f18f8f; color: darkred;"> NÃO É BISSEXTO</mark> \u{274C}`
+        leapYear = `<strong style = "background-color: #f18f8f; color: darkred;"> NÃO É BISSEXTO</strong> \u{274C}`
     }
     exit.innerHTML += `O ano de ${year} ${leapYear}`
 }
