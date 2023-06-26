0e4403a5c8516de863eb25d760554e72d5ff5ada
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-26 14:52:22 -0300

    Update script.js

--- desafios/d011/script.js
+++ desafios/d011/script.js
@@ -1,12 +1,13 @@
 function leapY() {
     let year = Number(prompt('Qual é o ano que você quer verificar?'))
     let exit = document.querySelector('p#exit')
-    let leapYear = ''
-    exit.innerHTML = `<h2>Analisando o ano de ${year}...</h2>`
-    if ((year % 4 == 0) && (year % 100 != 0) || (year % 100 == 0)) {
-        leapYear = `<strong style = "background-color: lightgreen; color: green;"> É BISSEXTO</strong> \u{2705}`
+    let lYear = ''
+    if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)) {
+        lYear = '<strong style = "background-color: #9ff89f; color: green">É BISSEXTO</strong> \u{2705}'
     } else {
-        leapYear = `<strong style = "background-color: #f18f8f; color: darkred;"> NÃO É BISSEXTO</strong> \u{274C}`
+        lYear = '<strong style = "background-color: #f17b7b; color: darkred">NÃO É BISSEXTO</strong> \u{274C}'
     }
-    exit.innerHTML += `O ano de ${year} ${leapYear}`
+    
+    exit.innerHTML = `<h2>Analisando o ano de ${year}...</h2>`
+    exit.innerHTML += `<p>O ano de ${year} ${lYear}</p>`
 }
