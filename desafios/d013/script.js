c10dccf314bf6d690b3aff55dfc474c23af5e8da
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-27 11:30:41 -0300

    Update script.js

--- desafios/d013/script.js
+++ desafios/d013/script.js
@@ -2,18 +2,15 @@ function average() {
     let name = prompt('Qual é o nome do aluno?')
     let grade1 = parseFloat(prompt(`Primeira nota de ${name}:`))
     let grade2 = parseFloat(prompt(`Segunda nota de ${name}:`))
-    let average = (grade1 + grade2)/ 2
-
+    let FinalA = (grade1+grade2)/ 2
     let exit = document.querySelector('p#exit')
-    exit.innerHTML = `<h2>As notas de ${name} foram ${(grade1).toFixed(1)} e ${(grade2).toFixed(1)}</h2>`
-    let situation = ''
-    if (average > 6) {
-        situation = 'foi <strong style = "color: green;">APROVADO!</strong>'
-    } else if (average < 6 && (average >= 3)) {
-        situation = 'ficou em <strong style = "color: orange;">RECUPERAÇÃO!</strong>'
+    result = ''
+    if (FinalA < 3) {
+        result += '<strong style = "color: red">REPROVADO</strong>'
+    } else if (FinalA >= 3 && (FinalA < 6)) {
+        result += 'em <strong style = "color: orange">RECUPERAÇÃO</strong>'
     } else {
-        situation = 'foi <strong style = "color: red;">REPROVADO!</strong>'
+        result += '<strong style = "color: green">APROVADO</strong>'
     }
-    exit.innerHTML += `<p>A média de ${name} foi ${(average).toFixed(1)}.</p>`
-    exit.innerHTML += `<p>Com a média ${average}, o(a) aluno(a) ${situation}</p>`
+    exit.innerHTML += `<p>Com a média ${FinalA}, o aluno ${name} está ${result}!</p>`
 }
