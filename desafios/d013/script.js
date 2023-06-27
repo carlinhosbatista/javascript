b2ac0fdad01875ea72d7320c05590f452fce6bba
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-27 11:38:21 -0300

    Update script.js

--- desafios/d013/script.js
+++ desafios/d013/script.js
@@ -6,11 +6,14 @@ function average() {
     let exit = document.querySelector('p#exit')
     result = ''
     if (FinalA < 3) {
-        result += '<strong style = "color: red">REPROVADO</strong>'
+        result = '<strong style = "color: red">REPROVADO</strong>'
     } else if (FinalA >= 3 && (FinalA < 6)) {
-        result += 'em <strong style = "color: orange">RECUPERAÇÃO</strong>'
+        result = 'em <strong style = "color: orange">RECUPERAÇÃO</strong>'
     } else {
-        result += '<strong style = "color: green">APROVADO</strong>'
+        result = '<strong style = "color: green">APROVADO</strong>'
     }
-    exit.innerHTML += `<p>Com a média ${FinalA}, o aluno ${name} está ${result}!</p>`
+    exit.innerHTML = `<h2>Analisando a situação do(a) aluno(a) ${name}.</h2>`
+    exit.innerHTML += `<p>A primeira nota de ${name} foi ${grade1.toFixed(1)} e a segunda nota foi ${grade2}.</p>`
+    exit.innerHTML += `<p>Com as notas ${grade1.toFixed(1)} e ${grade2.toFixed(1)}, a média de ${name} foi ${FinalA}.</p>`
+    exit.innerHTML += `<p>Com a média ${FinalA.toFixed(1)}, o aluno ${name} está ${result}!</p>`
 }
