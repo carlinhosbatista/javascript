9838a8aa38533351956426420c1b808ef2bd1c31
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-29 10:36:45 -0300

    Update script.js

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -2,16 +2,19 @@ function carregar() {
     let msg = document.querySelector('div#msg')
     let img = document.querySelector('img#imagem')
     let date = new Date()
-    let hour = date.getHours()
+    let hour = 19//date.getHours()
     msg.innerHTML = `Agora são ${hour} horas. `
     if (hour >= 0 && hour < 12) {
         msg.innerHTML += 'Bom dia!'
         img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
     } else if (hour >= 12 && hour < 18) {
         msg.innerHTML += 'Boa tarde!'
         img.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
     } else {
         msg.innerHTML += 'Boa noite!'
         img.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
     }
 }
