af290c5a3ff4fbbfe5c18d4d58cf5c4fdce4f68c
Carlos Batista <carlosbatistadev7@gmail.com>
2023-06-29 10:33:31 -0300

    Update script.js

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -1,21 +1,17 @@
 function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem') 
-    var data = new Date()   
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
-    if (hora >= 0 && hora < 12) {
-        // BOM DIA!
+    let msg = document.querySelector('div#msg')
+    let img = document.querySelector('img#imagem')
+    let date = new Date()
+    let hour = date.getHours()
+    msg.innerHTML = `Agora são ${hour} horas. `
+    if (hour >= 0 && hour < 12) {
+        msg.innerHTML += 'Bom dia!'
         img.src = 'fotomanha.png'
-        document.body.style.background = '#e2cd9f'
-    } else if (hora >= 12 && hora <= 18) {
-        // BOA TARDE!
+    } else if (hour >= 12 && hour < 18) {
+        msg.innerHTML += 'Boa tarde!'
         img.src = 'fototarde.png'
-        document.body.style.background = '#b98469'
     } else {
-        // BOA NOITE!
+        msg.innerHTML += 'Boa noite!'
         img.src = 'fotonoite.png'
-        document.body.style.background = '#515154'
     }
-    
 }
